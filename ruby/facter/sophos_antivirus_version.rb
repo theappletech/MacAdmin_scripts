86da78cb39065c301df78920a5f1a57baffc249c
Ben Snyder <ben@bensnyder.com>
2013-02-10 22:20:05 -0500

    Figured out what I was missing
    Needed to use grep -E

--- ruby/facter/sophos_antivirus_version.rb
+++ ruby/facter/sophos_antivirus_version.rb
@@ -2,7 +2,7 @@ Facter.add('sophos_antivirus_version') do
   confine :operatingsystem => :darwin
   setcode do
     if File.exists?('/usr/bin/sweep')
-      Facter::Util::Resolution.exec(/usr/bin/sweep -v | /usr/bin/grep "Product version" | /usr/bin/awk '{print $4}')
+      Facter::Util::Resolution.exec("/usr/bin/sweep -v | /usr/bin/grep -E 'Product version' | /usr/bin/awk '{ print $4 }'")
     end
   end
 end
