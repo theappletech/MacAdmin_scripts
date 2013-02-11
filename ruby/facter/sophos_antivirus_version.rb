e8a6c679302e47a38dd0309471bce2510e4fc4ce
Ben Snyder <ben@bensnyder.com>
2013-02-10 20:51:34 -0500

    Adding an attempt at a custom fact
    Detecting Sophos Antivirus Version

---
+++ ruby/facter/sophos_antivirus_version.rb
@@ -0,0 +1,8 @@
+Facter.add('sophos_antivirus_version') do
+  confine :operatingsystem => :darwin
+  setcode do
+    if File.exists?('/usr/bin/sweep')
+      Facter::Util::Resolution.exec(/usr/bin/sweep -v | /usr/bin/grep "Product version" | /usr/bin/awk '{print $4}')
+    end
+  end
+end
